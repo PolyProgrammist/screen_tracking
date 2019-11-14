5efa6e88ca7618f64e5573e74d0e8205ffb426f7
polyprogrammist <pechkin350@gmail.com>
2019-11-14 07:25:48 +0300

    good canny

--- screen_tracking/tracker/hough_heuristics/tracking.py
+++ screen_tracking/tracker/hough_heuristics/tracking.py
@@ -28,34 +28,10 @@ class Tracker:
     tracker_params = TrackerParams()
 
     FRAMES_NUMBER_TO_TRACK = np.inf
-    INITIAL_FRAME = 73
-    PREVIOUS_GROUND_TRUTH = False
+    INITIAL_FRAME = 87
+    PREVIOUS_GROUND_TRUTH = True
 
     SHOW_EACH_SIDE = int(1e9)
-
-    def rectangle_frontiers(self, side_frontiers_in, side_frontiers_out):
-        in_frontier = RectFrontier(side_frontiers_in)
-        in_frontier.print_best()
-        in_frontier = PreviousPoseFrontier(in_frontier)
-        in_frontier.print_best()
-        in_frontier = PNPrmseFrontier(in_frontier)
-        in_frontier.print_best()
-        in_frontier = SquareFrontier(in_frontier)
-        in_frontier.print_best()
-        in_frontier = RectangleGradientFrontier(in_frontier)
-        in_frontier.print_best()
-        in_frontier = OuterVarianceFrontier(in_frontier)
-        in_frontier.print_best()
-        in_frontier = AspectRatioFrontier(in_frontier)
-        in_frontier.print_best()
-
-        out_frontier = RectFrontier(side_frontiers_out)
-        out_frontier = PreviousPoseFrontier(out_frontier)
-        out_frontier = PNPrmseFrontier(out_frontier)
-
-        in_frontier.print_best(caption='In frontier')
-        out_frontier.print_best(caption='Out frontier')
-        return in_frontier, out_frontier
 
     def line_frontiers(self):
         last_lines = screen_points_to_lines(self.state.last_points)
@@ -63,8 +39,11 @@ class Tracker:
         hough_frontier = HoughFrontier(self)
         side_frontiers_out = [hough_frontier for _ in last_lines]
 
-        side_frontiers_out = [GroundTruthLineFrontier(frontier, i) for i, frontier in enumerate(side_frontiers_out)]
-        self.show_list_best(side_frontiers_out)
+        # my_frontiers = [GroundTruthLineFrontier(frontier, i) for i, frontier in enumerate(side_frontiers_out)]
+        # for frontier in my_frontiers:
+        #     if len(frontier.top_current()) == 0:
+        #         self.show_list_best(my_frontiers)
+        # return None, None
 
         side_frontiers_out = [PhiFrontier(frontier, last_line) for frontier, last_line in
                               zip(side_frontiers_out, last_lines)]
@@ -76,9 +55,36 @@ class Tracker:
         side_frontiers_in = [PreviousLineDistanceFrontier(frontier, last_line, inner=True) for frontier, last_line in
                              zip(side_frontiers_out, last_lines)]
 
-        self.show_list_best(side_frontiers_out)
+        # self.show_list_best(side_frontiers_out)
 
         return side_frontiers_in, side_frontiers_out
+
+    def rectangle_frontiers(self, side_frontiers_in, side_frontiers_out):
+        in_frontier = RectFrontier(side_frontiers_in)
+        in_frontier = GroundTruthFrontier(in_frontier)
+        show_best(in_frontier)
+        in_frontier.print_best()
+        in_frontier = PreviousPoseFrontier(in_frontier)
+        in_frontier.print_best()
+        in_frontier = PNPrmseFrontier(in_frontier)
+        in_frontier.print_best()
+        in_frontier = SquareFrontier(in_frontier)
+        in_frontier.print_best()
+        in_frontier = RectangleGradientFrontier(in_frontier)
+        in_frontier.print_best()
+        in_frontier = OuterVarianceFrontier(in_frontier)
+        in_frontier.print_best()
+        show_best(in_frontier, show_all=True)
+        in_frontier = AspectRatioFrontier(in_frontier)
+        in_frontier.print_best()
+
+        out_frontier = RectFrontier(side_frontiers_out)
+        out_frontier = PreviousPoseFrontier(out_frontier)
+        out_frontier = PNPrmseFrontier(out_frontier)
+
+        in_frontier.print_best(caption='In frontier')
+        out_frontier.print_best(caption='Out frontier')
+        return in_frontier, out_frontier
 
     def in_out_frontier(self, in_frontier, out_frontier):
         in_out_frontier = InOutFrontier(in_frontier, out_frontier)
@@ -102,10 +108,17 @@ class Tracker:
         self.state.predict_matrix = predict_matrix
 
         side_frontiers_in, side_frontiers_out = self.line_frontiers()
+        if side_frontiers_in is None:
+            return None
         in_frontier, out_frontier = self.rectangle_frontiers(side_frontiers_in, side_frontiers_out)
+        if in_frontier is None:
+            return None
         in_out_frontier = self.in_out_frontier(in_frontier, out_frontier)
+        if out_frontier is None:
+            return None
         rect_frontier = self.rectangle_after_in_out(in_out_frontier)
-
+        if rect_frontier is None:
+            return None
         # show_best(rect_frontier, show_all=True)
 
         rect_frontier = rect_frontier.top_current()[0]
@@ -133,15 +146,15 @@ class Tracker:
                 frame_number += 1
                 print('\nFrame number: ', frame_number)
                 self.state.frame_number = frame_number
-                # TODO: remove ground truth
                 ret, frame = cap.read()
                 if not ret:
                     break
                 points = self.get_points(frame, last_frame[0], last_points[0], predict_matrix)
-                self.write_camera(tracking_result, points, frame_number)
+                if points is not None:
+                    self.write_camera(tracking_result, points, frame_number)
+                    predict_matrix = tracking_result[frame_number]
 
-                predict_matrix = tracking_result[frame_number]
-
+                # TODO: remove ground truth
                 if self.PREVIOUS_GROUND_TRUTH:
                     _, rotation, translation = cv2.solvePnP(
                         self.model_vertices,
