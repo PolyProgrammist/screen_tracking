7a084807d30ebf4d39355dc1df4d65713c7e8447
polyprogrammist <pechkin350@gmail.com>
2020-01-30 17:02:17 +0300

    bohdan integrated

--- screen_tracking/tracker/lukas_kanade/lukas_kanade.py
+++ screen_tracking/tracker/lukas_kanade/lukas_kanade.py
@@ -2,12 +2,186 @@ import logging
 
 import numpy as np
 import cv2
+from scipy.optimize import least_squares
 
 from screen_tracking.tracker.common_tracker import Tracker
 from screen_tracking.tracker.hough_heuristics.utils import get_screen_points, screen_points_to_lines, draw_line
 from screen_tracking.tracker.hough_heuristics.utils.draw import show_frame
 from screen_tracking.tracker.lukas_kanade.lukas_params import TrackerParams
 
+
+
+def to_mat(vec):
+    a, sx, sy, m, tx, ty = vec
+    r_mat = np.array([
+        [np.cos(a), -np.sin(a)],
+        [np.sin(a), np.cos(a)]
+    ])
+    shear_mat = np.array([
+        [1.0, m],
+        [0.0, 1.0]
+    ])
+    scale_mat = np.array([
+        [sx, 0],
+        [0, sy]
+    ])
+    mat = np.zeros((2, 3))
+    mat[:, :2] = r_mat @ shear_mat @ scale_mat
+    mat[:, 2] = np.array([tx, ty])
+    return mat
+
+    # rotation + translation
+    # return np.array([
+    #     [np.cos(a), -np.sin(a), vec[1]],
+    #     [np.sin(a), np.cos(a), vec[2]]
+    # ])
+
+
+def from_mat(mat):
+    tx, ty = mat[:, 2]
+    a = mat[:, :2]
+    sx = (a[0, 0] ** 2 + a[1, 0] ** 2) ** 0.5
+    angle = np.arctan(a[1, 0] / a[0, 0])
+    msy = a[0, 1] * np.cos(angle) + a[1, 1] * np.sin(angle)
+    if np.isclose(angle, 0):
+        sy = (a[1, 1] - msy * np.sin(angle)) / np.cos(angle)
+    else:
+        sy = (msy * np.cos(angle) - a[0, 1]) / np.sin(angle)
+    m = msy / sy
+    return np.array([angle, sx, sy, m, tx, ty])
+
+    # rotation + translation
+    # return np.array([np.arcsin(mat[1, 0]), mat[0, 2], mat[1, 2]])
+
+
+def track(patch_size, img_0, img_1, pose_0, pose_1=None, verbose=0):
+    assert(img_0.shape == img_1.shape)
+    img_size = img_0.shape[1], img_1.shape[0]
+
+    img_0_warped = cv2.warpAffine(img_0, pose_0, img_size)
+
+    rs, cs = patch_size
+
+    def func(x):
+        h = to_mat(x + from_mat(pose_0))
+        img_1_warped = cv2.warpAffine(img_1, h, img_size)
+        residuals = (img_1_warped[:rs, :cs] - img_0_warped[:rs, :cs]).flatten()
+        # print((residuals ** 2).sum())
+        return residuals
+
+    if pose_1 is None:
+        pose_1 = pose_0
+    x0 = from_mat(pose_1) - from_mat(pose_0)
+    result = least_squares(
+        func,
+        x0,
+        diff_step=np.array([0.0001, 0.001, 0.001, 0.001, 0.1, 0.1]),
+        jac='3-point',
+        xtol=1e-3,
+        gtol=1e-3,
+        ftol=1e-3,
+        verbose=verbose
+    )
+
+    return to_mat(result.x + from_mat(pose_0))
+
+
+def show_diff(img_0, img_1, pose_0, pose_1):
+    img_0_warped = cv2.warpAffine(img_0, pose_0, (img_0.shape[1], img_0.shape[0]))
+    img_1_warped = cv2.warpAffine(img_1, pose_1, (img_1.shape[1], img_1.shape[0]))
+    img_diff = np.abs(img_0_warped - img_1_warped)
+    cv2.imshow('Diff', img_diff)
+    cv2.waitKey(0)
+
+
+def downscale_pose(pose_mat, lvl):
+    new_pose = pose_mat.copy()
+    new_pose[:, 2] /= 2 ** lvl
+    return new_pose
+
+
+def track_pyr(patch_size, img_0, img_1, pose_0, lvl_count=3, verbose=0):
+    pyramid_0 = [img_0]
+    pyramid_1 = [img_1]
+    for lvl in range(lvl_count - 1):
+        pyramid_0.append(cv2.pyrDown(pyramid_0[-1]))
+        pyramid_1.append(cv2.pyrDown(pyramid_1[-1]))
+
+    pose = downscale_pose(pose_0, lvl_count - 1)
+    for lvl in range(lvl_count - 1, -1, -1):
+        pose_opt = track(
+            patch_size,
+            pyramid_0[lvl],
+            pyramid_1[lvl],
+            downscale_pose(pose_0, lvl),
+            pose,
+            verbose
+        )
+        pose = pose_opt
+        if lvl > 0:
+            pose[:, 2] *= 2
+    return pose
+
+
+def imgname(number):
+    number = str(number)
+    number = '0' * (4 - len(number)) + number
+    result = 'card/' + number + '.jpg'
+    return result
+
+
+def inverse_pose(y, P):
+    A = P[:, :2]
+    t = P[:, 2:]
+    y = np.array(y).reshape(-1, 1)
+    x = np.linalg.inv(A) @ (y - t)
+    return x.reshape(-1)
+
+
+def show_rectangle(img, patch_size, pose):
+    points = [
+        [0, 0],
+        [0, patch_size[1]],
+        [patch_size[0], patch_size[1]],
+        [patch_size[0], 0]
+    ]
+    result = []
+    for point in points:
+        result.append(inverse_pose(point, pose))
+
+    np.array(result, np.int32)
+    img = img.copy()
+    cv2.polylines(img, np.array([result], np.int32), True, (0, 255, 255))
+    # cv2.imshow('rect', img)
+    # cv2.waitKey(0)
+    return img
+
+def convert_to_track(img):
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = img / 255.0
+    return img.astype(np.float32)
+
+def bohdan_tracking(cur_frame, last_frame, last_points):
+    img_0 = convert_to_track(last_frame)
+    img_1 = convert_to_track(cur_frame)
+    patch_size = [101, 101]
+
+    ret_points = []
+    for i in range(4):
+        point = last_points[i]
+        translation = -(point - np.array(patch_size) / 2.0)
+        pose_0 = np.array([
+            [1, 0, translation[0]],
+            [0, 1, translation[1]]
+        ])
+        pose_1 = track(patch_size, img_0, img_1, pose_0)
+        to_invert = np.array(patch_size) / 2
+        point = inverse_pose(to_invert, pose_1)
+        ret_points.append(point)
+
+    ret_points = np.array(ret_points)
+    print(ret_points)
+    return ret_points
 
 class LukasKanadeTracker(Tracker):
     tracker_params = TrackerParams()
@@ -16,17 +190,4 @@ class LukasKanadeTracker(Tracker):
         super().__init__(model_vertices, camera_params, video_source, frame_pixels)
 
     def get_points(self, cur_frame, last_frame, last_points, predict_matrix):
-        last_points = np.array([[point] for point in last_points], dtype=np.float32)
-        lk_params = dict(winSize=(70, 70),
-                         maxLevel=4,
-                         criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 100, 0.001))
-
-        color = np.random.randint(0, 255, (100, 3))
-
-        old_gray = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
-
-        frame = cur_frame
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, last_points, None, **lk_params)
-        good_new = p1[st == 1]
-        return good_new
+        return bohdan_tracking(cur_frame, last_frame, last_points)
